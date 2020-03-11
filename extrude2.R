08c3d5b0a2f253b73e0fc25be91aa069c5720809
Nicolas Lambert <nicolas.lambert@cnrs.fr>
2020-03-11 17:42:30 +0100

    order extruded polygons - resolved in extrude2.R

--- extrude2.R
+++ extrude2.R
@@ -2,76 +2,76 @@ library(sf)
 library(scales)
 
 
-# original sf
-
-states <- readRDS("us.rds")
-crs <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs "
-states <- st_transform(states, crs)
-plot(st_geometry(states))
-
-# vintage frame
-
-frame <- function(x){
-  bb <- st_bbox(x)
-  d <- (bb[3] - bb[1]) / 50
-  bb <- bb + c(-d, -d, d, d)
-  frame <- st_as_sfc(bb, crs = st_crs(x))
-  f <- st_segmentize(frame, d)
-  f <- st_cast(f, "POINT")
-  plot(f, pch=3)
-}
-
-# frame(states)
-# plot(st_geometry(states), add=T)
-
-# get frame
-
-getframe <- function(x){
-  bb <- st_bbox(x)
-  d <- (bb[3] - bb[1]) / 50
-  bb <- bb + c(-d, -d, d, d)
-  frame <- st_as_sfc(bb, crs = st_crs(x))
-  frame <- st_sf(geometry = frame)
-  return(frame)
-}
-
-# f <- getframe(states)
-# plot(st_geometry(states))
-# plot(st_geometry(f), add=T)
-
-
-# centroid
-
-center <- function(x){
-  x <- st_as_sfc(st_bbox(x))
-  x <- st_sf(geometry = st_centroid(x))
-  x$x <- st_coordinates(x)[1]
-  x$y <- st_coordinates(x)[2]
-  x <- x[,c("x","y","geometry")]
+  # original sf
+  
+  states <- readRDS("us.rds")
+  crs <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs "
+  states <- st_transform(states, crs)
+  plot(st_geometry(states))
+  
+  # vintage frame
+  
+  frame <- function(x){
+    bb <- st_bbox(x)
+    d <- (bb[3] - bb[1]) / 50
+    bb <- bb + c(-d, -d, d, d)
+    frame <- st_as_sfc(bb, crs = st_crs(x))
+    f <- st_segmentize(frame, d)
+    f <- st_cast(f, "POINT")
+    plot(f, pch=3)
+  }
+  
+  # frame(states)
+  # plot(st_geometry(states), add=T)
+  
+  # get frame
+  
+  getframe <- function(x){
+    bb <- st_bbox(x)
+    d <- (bb[3] - bb[1]) / 50
+    bb <- bb + c(-d, -d, d, d)
+    frame <- st_as_sfc(bb, crs = st_crs(x))
+    frame <- st_sf(geometry = frame)
+    return(frame)
+  }
+  
+  # f <- getframe(states)
+  # plot(st_geometry(states))
+  # plot(st_geometry(f), add=T)
+  
+  
+  # centroid
+  
+  center <- function(x){
+    x <- st_as_sfc(st_bbox(x))
+    x <- st_sf(geometry = st_centroid(x))
+    x$x <- st_coordinates(x)[1]
+    x$y <- st_coordinates(x)[2]
+    x <- x[,c("x","y","geometry")]
+    return(x)
+   }
+  
+  # c <- center(states)
+  # plot(st_geometry(states))
+  # plot(st_geometry(c), add=TRUE, pch=20, lwd = 10)
+  
+  
+  # Rotate
+  
+  rotate <- function(x, deg){
+  geom <- st_geometry(x) 
+  rot = function(a) matrix(c(cos(a), sin(a), -sin(a), cos(a)), 2, 2)   
+  ctr <- st_centroid(st_as_sfc(st_bbox(geom)))
+  geom <- (geom - ctr) * rot(deg * pi/180) + ctr                     
+  st_geometry(x) <- geom
   return(x)
- }
-
-# c <- center(states)
-# plot(st_geometry(states))
-# plot(st_geometry(c), add=TRUE, pch=20, lwd = 10)
-
-
-# Rotate
-
-rotate <- function(x, deg){
-geom <- st_geometry(x) 
-rot = function(a) matrix(c(cos(a), sin(a), -sin(a), cos(a)), 2, 2)   
-ctr <- st_centroid(st_as_sfc(st_bbox(geom)))
-geom <- (geom - ctr) * rot(deg * pi/180) + ctr                     
-st_geometry(x) <- geom
-return(x)
-}
-
-# x <- rotate(states, 0)
-# plot(st_geometry(x))
-
-# Deform -----------------------------------------------------------------------------------
-
+  }
+  
+  # x <- rotate(states, 0)
+  # plot(st_geometry(x))
+  
+  # Deform -----------------------------------------------------------------------------------
+  
 deform <- function(x, flatten = 0.8, rescale = c(1.3,2), rotate = 0){
 
 # x = states # enlever  
@@ -145,14 +145,22 @@ x <- deform(states)
 f <- deform(getframe(states))
 plot(st_geometry(f))
 plot(st_geometry(x), add=T)
+# View(x)
+#x <- x[x$id %in% c("CA", "AZ"),]
 
 # EXTRUDE
 
+x <- deform(states)
+var = "pop2019"
+k = 0.5
+col = "red"
+add = FALSE
 extrude <- function(x, var, k = 0.5, col = "red", add = FALSE) {
   
   # multi polygons -> polygons
   x <- st_cast(x, "POLYGON")
-  
+  x$id <- row.names(x)
+
   # colors
   if(col == "white"){
   fill <- c("white","white","white") 
@@ -167,12 +175,14 @@ extrude <- function(x, var, k = 0.5, col = "red", add = FALSE) {
   x$order <- st_coordinates(st_centroid(st_geometry(x)))[,2]
   x <- x[order(x$order, decreasing = TRUE),]
   
-  plot(st_geometry(x), col="#CCCCCC", border="white",add = add)
+  # plot(st_geometry(x), col="#CCCCCC", border="white",add = add)
   
   for (i in 1:nrow(x)){
+
     height <-  data.frame(x[i,var])[1]
     poly <- st_geometry(x)[[i]]
     poly2 <- poly + c(0, as.numeric(x[i,var])[1] * k)
+    top <- st_sf(x[i,], geometry = st_sfc(poly2))
     pts <- st_coordinates(poly)
     pts2 <- st_coordinates(poly2)
     nb <- dim(pts)[1] - 1
@@ -188,65 +198,100 @@ extrude <- function(x, var, k = 0.5, col = "red", add = FALSE) {
       pos <- as.numeric((dot1["Y"] +  dot2["Y"]) / 2)
       ang <- atan((dot2["Y"] - dot1["Y"]) / ( dot2["X"] - dot1["X"]))*180/pi
       if (z == 1 & i == 1){
-        faces <- st_sf(pos = pos, ang = ang, fill = fill[2], border = border[2], height = height, geometry=st_sfc(st_polygon(list(rbind(dot1, dot2, dot3, dot4, dot1)))))
-      } else {
-        face <- st_sf(pos = pos, ang = ang, fill = fill[2], border = border[2], height = height, geometry=st_sfc(st_polygon(list(rbind(dot1, dot2, dot3, dot4, dot1)))))
-        faces <- rbind(faces, face)
-      }
-      }
-    }
+        faces <- st_sf(id = x$id[i], pos = pos, ang = ang, fill = fill[2], border = border[2], height = height, geometry=st_sfc(st_polygon(list(rbind(dot1, dot2, dot3, dot4, dot1)))))
 
-  
+      } else {
+        face <- st_sf(id = x$id[i], pos = pos, ang = ang, fill = fill[2], border = border[2], height = height, geometry=st_sfc(st_polygon(list(rbind(dot1, dot2, dot3, dot4, dot1)))))
+        faces <- rbind(faces, face)
+
+      }
+      }
+    
+    if (i == 1){
+      tops <- top
+    } else {
+      tops <- rbind(tops,top)
+    }
+    
+  }
+    
   faces <- faces[order(faces$pos, decreasing = TRUE),]
   faces$fill <- as.character(faces$fill)
   faces$border <- as.character(faces$border)
   faces[faces$ang > 0,"fill"] <- fill[3]   
   faces[faces$ang > 0,"border"] <- border[3]   
-  plot(st_geometry(faces), col=faces$fill, border = faces$border, lwd=0.4, add=TRUE)
-  plot(st_geometry(poly2), col=fill[1], border = border[1], add= TRUE)
+  #plot(st_geometry(faces), col=faces$fill, border = faces$border, lwd=0.4, add=TRUE)
+  # plot(st_geometry(poly2), col=fill[1], border = border[1], add= TRUE)
   
-  return(faces)
+  return(list(faces,tops))
 }
 
-plot(deform(getframe(states)))
-tmp <- extrude(x, "pop2019", k = 0.01, col ="#f0a800",add=T)
-plot(st_geometry(tmp))
-View(tmp)
-# TEST
 
-
-polys <- x[x$id %in% c("CA","AZ"),]
 View(x)
-plot(st_geometry(polys))
-extrude(polys, "pop2019", k = 0.01, col ="#f0a800",add=T)
+test <- extrude(x[x$id %in% c("CA","NV","AZ"),], "pop2019", k = 0.01, col ="#f0a800",add=T)
+# test <- extrude(x, "pop2019", k = 0.01, col ="#f0a800",add=T)
+faces <- test[[1]]
+tops <- test[[2]]
+
+faces$dbl <- duplicated(faces$pos)
+pos <- faces[faces$dbl == TRUE, "pos"]
+st_geometry(pos) <- NULL
+
+# Faces interieures
+
+for (i in 1:length(pos[,1])){
+  p = pos[i,1]
+  vars <- faces[faces$pos == p,var]
+  st_geometry(vars) <- NULL
+  x1 <- faces[faces$pos == p & faces$pop2019 == max(vars),]
+  x2 <- faces[faces$pos == p & faces$pop2019 == min(vars),]
+  f <- st_difference(x1,x2)
+  if(i == 1){int <- f} else {int <- rbind(int,f)}
+}
+
+# Faces exterieurs
+
+for (i in 1:nrow(tops)){
+  id = tops[i,"id"]
+  st_geometry(id) <- NULL
+  ok <- faces[faces$id == id$id,]
+  ok <- st_difference(ok,tops[i,])
+  if (i == 1) {
+    ext <- ok
+  } else {
+    ext <- rbind(ext,ok)
+  }
+}
 
 
+ids <-tops[order(tops$pop2019, decreasing = FALSE),"id"]
+st_geometry(ids) <- NULL
+ids <- ids$id
 
-# TEST SUR LA POP ##############################################################
+plot(st_geometry(x[x$id %in% c("CA","NV","AZ"),]))
 
-world <- readRDS("worldpopgrid.rds")
-frame <- getframe(world)
-plot(st_geometry(frame))
-plot(st_geometry(world), add=T)
+i = 1
 
-f <- deform(frame)
-x <- deform(world)
-plot(st_geometry(f))
-plot(st_geometry(x),add=T)
-x <- x[!is.na(x$pop2020),]
-plot(st_geometry(f))
-extrude(x, "pop2020", k = 0.02, col ="#656cd6",add=T)
+p1_a <- ext[ext$id == ids[i], ]
+p1_b <- int[int$id == ids[i], ]
+p1_c <- tops[tops$id == ids[i],]
+plot(st_geometry(p1_b), col = "green", add=T)
+plot(st_geometry(p1_c), col = "yellow", add=T)
+plot(st_geometry(p1_a), col = "blue", add= T)
+i = 2
 
-# TESTS DE TRI
+p1_a <- ext[ext$id == ids[i], ]
+p1_b <- int[int$id == ids[i], ]
+p1_c <- tops[tops$id == ids[i],]
+plot(st_geometry(p1_b), col = "green", add=T)
+plot(st_geometry(p1_c), col = "yellow", add=T)
+plot(st_geometry(p1_a), col = "blue", add= T)
 
-x <- deform(states)
-plot(st_geometry(x))
-c <- st_centroid(x)
-plot(st_geometry(c), pcg=21, cex =1, add=T)
+i = 3
 
-
-
-
-
-
-
+p1_a <- ext[ext$id == ids[i], ]
+p1_b <- int[int$id == ids[i], ]
+p1_c <- tops[tops$id == ids[i],]
+plot(st_geometry(p1_b), col = "green", add=T)
+plot(st_geometry(p1_c), col = "yellow", add=T)
+plot(st_geometry(p1_a), col = "blue", add= T)
